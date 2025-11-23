00f4e2cea349d5efb5ef537535b6f95079e13f29
abunusaybah <your_github_email@example.com>
2025-11-23 08:56:10 +0100

    Completed task 1

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Welcome to Splash App</h1>
+			<p>
+				Your one-stop platform for all your needs. Connecting people, creating
+				opportunities.
+			</p>
+		</div>
+	);
+};
+
+export default Home;

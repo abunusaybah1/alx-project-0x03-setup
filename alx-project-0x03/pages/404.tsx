a788a9386bd2a297ec49cd800e4f52e1450b3158
abunusaybah <your_github_email@example.com>
2025-11-26 14:33:45 +0100

    Added 404 page

--- alx-project-0x03/pages/404.tsx
+++ alx-project-0x03/pages/404.tsx
@@ -4,19 +4,17 @@ import { FaHome } from "react-icons/fa";
 const Custom404 = () => {
 	return (
 		<div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col justify-center items-center text-white">
-			{/* Main Heading */}
-			<h1 className="text-6xl font-bold mb-4">Oops!</h1>
+			
+            <h1 className="text-6xl font-bold mb-4">Oops!</h1>
 			<p className="text-2xl mb-8 max-w-md text-center">
 				We can&apos;t seem to find the page you&apos;re looking for.
 			</p>
 
-			{/* Funny Message */}
 			<p className="text-lg mb-8 text-center max-w-md">
 				Maybe it was abducted by aliens, or it just took a wrong turn into the
 				Internet wilderness! Either way, it&apos;s not here.
 			</p>
 
-			{/* Button to navigate back */}
 			<Link
 				href="/"
 				className="flex items-center space-x-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg transition duration-300">
